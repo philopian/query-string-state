91f4a25f4639c29fd70ffa01431b44cdb219a24f
Phil Willis <philopian@gmail.com>
2023-10-19 12:07:34 -0700

    path alias

---
+++ src/utils/path-extra.ts
@@ -0,0 +1,5 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+export const __filename = fileURLToPath(new URL(import.meta.url))
+export const __dirname = path.dirname(__filename)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+import { __dirname } from './src/utils/path-extra'
+
+const pathAlias = {
+  '@/': resolve(__dirname, './src'),
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      ...pathAlias,
+    },
+  },
 })
